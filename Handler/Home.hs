37d640c1d29eabbd7db13b09bed3cdba416257c6
DmLitov4 <dmitriyonline222@gmail.com>
2016-11-28 20:00:26 +0300

    Landing page for service is almost done.

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -25,7 +25,7 @@ getHomeR = do
     defaultLayout $ do
         let (commentFormId, commentTextareaId, commentListId) = commentIds
         aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
+        setTitle "Cafe Adviser"
         $(widgetFile "homepage")
 
 postHomeR :: Handler Html
