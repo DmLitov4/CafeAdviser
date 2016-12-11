350efb0537d9f672cdbd489a1f104f0505460646
Tananakin Mihail <tananakin.mihail@yandex.ru>
2016-12-11 03:04:25 +0300

    Insert restaurants module

--- Handler/RestAdd.hs
+++ Handler/RestAdd.hs
@@ -11,7 +11,7 @@ data AddRestForm = AddRestForm
        name    :: Text  --name of restaurant
     ,  kind    :: Text  --type is cafe, bar or restaurant
     ,  cuisine :: Text
-    ,  bill    :: Double
+    ,  bill    :: Int
     ,  city    :: Text
     ,  area    :: Text
     ,  feature :: Text
@@ -28,7 +28,7 @@ addForm = renderBootstrap3 BootstrapBasicForm $ AddRestForm
     <$> areq textField        "Название заведения  " Nothing
     <*> areq (selectFieldList [("ресторан" :: Text, "ресторан"),("кафе", "кафе"), ("бар", "бар")]) "Тип заведения:  " Nothing
     <*> areq (selectFieldList [("русская" :: Text, "русская"),("французская", "французская"), ("итальянская", "итальянская"), ("США", "США"), ("японская", "японская"), ("латиноамериканская", "латиноамериканская"), ("грузинская", "грузинская"), ("индийская", "индийская")]) "Предпочитаемая кухня:  " Nothing
-    <*> areq doubleField      "Средний чек: " Nothing
+    <*> areq intField      "Средний чек: " Nothing
     <*> areq (selectFieldList [("Ростов-на-Дону" :: Text, "Ростов-на-Дону")]) "Город:   " Nothing
     <*> areq (selectFieldList [("Центр" :: Text, "Центр"), ("Западный", "Западный"), ("Северный", "Северный"), ("Александровка", "Александровка"), ("Сельмаш", "Сельмаш")]) "Район:  " Nothing
     <*> areq (selectFieldList [("День Рождения" :: Text, "День Рождения"), ("свадьба", "свадьба"), ("корпоратив", "корпоратив")]) "Подходит для:  " Nothing
@@ -50,12 +50,28 @@ postRestAddR :: Handler Html
 postRestAddR = do
     ((result, formWidget), formEnctype) <- runFormPost addForm
     case result of
-      FormSuccess newRest -> addToDB newRest
+      FormSuccess newRest -> do
+                          --Insert data in Kinds, Cuisines, Cities, Areas and Features tables--
+                          kindId <- runDB $ insert $ Kinds (kind newRest)                    --kind id in Kinds Table
+                          cuisineId <- runDB $ insert $ Cuisines (cuisine newRest)            --cuisin id in Cuisines Table
+                          cityId <- runDB $ insert $ Cities (city newRest)                   --city id in Cities Table
+                          areaId <- runDB $ insert $ Areas (area newRest)                    --area id in Areas Table
+                          featureId <- runDB $ insert $ Features (feature newRest)           --feature id in Features Table
+
+                          --Now we inserts new restaurant in Restaurants table--
+                          restId <- runDB $ insert $ Restaurants (name newRest) (kindId) (cuisineId) (bill newRest) (cityId) (areaId) (featureId) (boolToInt (parking newRest) ) (boolToInt (dancing newRest) ) (boolToInt (garden newRest) ) (description newRest) (image newRest)
+                          maybeRest <- runDB $ get restId
+                          case maybeRest of
+                              Nothing -> error "insert error"
+                              _ -> error "Ok"
+
+                          defaultLayout $ do
+                            setTitle "Cafe Adviser - добавить заведение"
+                            $(widgetFile "addrest")
+
       _ -> defaultLayout $ do
              setTitle "Cafe Adviser - добавить заведение"
              $(widgetFile "addrest")
 
-
---Here we add new restaurants to Data Base
-addToDB :: AddRestForm -> Handler Html
-addToDB rf = undefined
+boolToInt :: Bool -> Int
+boolToInt t = if t then 1 else 0
